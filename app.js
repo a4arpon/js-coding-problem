ae6311e4e37b5ad25f241ad47edb814aca1364fc
Mr Wayne <a4arpon@gmail.com>
2023-07-25 23:49:40 +0600

    2nd and 3rd problem

--- app.js
+++ app.js
@@ -5,7 +5,37 @@ const stringRev = (string) => {
   for (let i = stringArr.length - 1; i >= 0; i--) {
     revString += stringArr[i]
   }
-  console.log(revString)
   return revString
 }
-console.log(stringRev('hello world') === 'dlrow olleh')
+console.log(stringRev('hello world'))
+
+// Problem 2: Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array.
+
+const sumArray = (arr) => {
+  let sumOfPositive = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sumOfPositive = sumOfPositive + arr[i]
+    }
+  }
+  return sumOfPositive
+}
+console.log(sumArray([2, -5, 10, -3, 7]))
+
+// Problem 3: Write a JavaScript program to find the most frequent element in an array and return it.
+const freqEl = (arr) => {
+  const freqCount = arr.reduce((counter, el) => {
+    counter[el] = (counter[el] || 0) + 1
+    return counter
+  }, {})
+  let mostFreqEl
+  let maxFreq = 0
+  for (const el in freqCount) {
+    if (freqCount[el] > maxFreq) {
+      mostFreqEl = el
+      maxFreq = freqCount[el]
+    }
+  }
+  return mostFreqEl
+}
+console.log(freqEl([3, 5, 2, 5, 3, 3, 1, 4, 5, 5]))
