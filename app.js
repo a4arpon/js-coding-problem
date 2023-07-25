9589ce3708992a26502430634bec2c3e9ba31561
Mr Wayne <a4arpon@gmail.com>
2023-07-25 19:07:40 +0600

    Problem One Solved

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+// Problem 1: Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+const stringRev = (string) => {
+  let revString = ''
+  const stringArr = string.split('')
+  for (let i = stringArr.length - 1; i >= 0; i--) {
+    revString += stringArr[i]
+  }
+  console.log(revString)
+  return revString
+}
+console.log(stringRev('hello world') === 'dlrow olleh')
