cd661336094aad51b9ed4f4402f1fcdd69e03022
Mr Wayne <a4arpon@gmail.com>
2023-07-26 20:13:22 +0600

    prblem 5 solved

--- app.js
+++ app.js
@@ -54,3 +54,33 @@ const passwordGen = (passLength) => {
 }
 
 console.log(passwordGen(12))
+
+// Problem 5: Implement a function that converts a Roman numeral to an integer. The function should take a Roman numeral string (e.g., "IX" or "XXI") as input and return the corresponding integer value.
+
+const romanToInt = (romanNum) => {
+  const romanSym = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
+
+  let result = 0
+  let prevValue = 0
+
+  for (let i = romanNum.length - 1; i >= 0; i--) {
+    const currentSymbol = romanNum[i]
+    const currentValue = romanSym[currentSymbol]
+    if (currentValue < prevValue) {
+      result -= currentValue
+    } else {
+      result += currentValue
+    }
+    prevValue = currentValue
+  }
+  return result
+}
+console.log(romanToInt('XXI'))
